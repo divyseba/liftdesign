60d9e4ff6375e6f3db30b13fd33a10c7698f4952
divyseba <104776841+divyseba@users.noreply.github.com>
2022-05-31 13:32:12 +0530

    Create LiftOEM.java

---
+++ springboot/src/main/java/com/example/sicap/springboot/LiftOEM.java
@@ -0,0 +1,21 @@
+package com.example.sicap.springboot;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LiftOEM {
+	
+	public void moveUp() {}
+	
+	public void moveDown() {}
+	
+	public void stop() {}
+	
+	public void openDoor() {}
+	
+	public void closeDoor() {}
+	
+	public void callBack() {}
+	
+
+}
